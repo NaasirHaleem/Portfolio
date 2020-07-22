7eb9c0871aceb5c3fb6598f65f70c73e484e3db4
NaasirHaleem <49250047+NaasirHaleem@users.noreply.github.com>
2020-07-22 16:01:08 -0500

    Delete AmazonCustomJuly20192020SQLQuery_1.sql

--- AmazonCustomJuly20192020SQLQuery_1.sql
+++
@@ -1,9 +0,0 @@
--- Select * From [AmazonCustomTransaction07162019-07152020]
-
-UPDATE [AmazonCustomTransaction07162019-07152020]
-
-SET date_time = LEFT(date_time,12)
-
-Where settlement_id > 1
-
-Select * From [AmazonCustomTransaction07162019-07152020]
